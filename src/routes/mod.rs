ff4dad688616ddb35f145ed596f466e39568e0a4
namwonkim <namwon94@gamil.com>
2025-08-16 15:11:24 +0900

    renamed:    src/routes/guess/guess.rs -> src/routes/ch2_guess/guess.rs 	renamed:    src/routes/guess/mod.rs -> src/routes/ch2_guess/mod.rs 	renamed:    src/routes/general_def/arrary.rs -> src/routes/ch3_general_def/arrary.rs 	renamed:    src/routes/general_def/control.rs -> src/routes/ch3_general_def/control.rs 	renamed:    src/routes/general_def/function.rs -> src/routes/ch3_general_def/function.rs 	renamed:    src/routes/general_def/mod.rs -> src/routes/ch3_general_def/mod.rs 	renamed:    src/routes/general_def/study_fibonacci.rs -> src/routes/ch3_general_def/study_fibonacci.rs 	renamed:    src/routes/general_def/study_temperature.rs -> src/routes/ch3_general_def/study_temperature.rs 	renamed:    src/routes/general_def/variant.rs -> src/routes/ch3_general_def/variant.rs 	renamed:    src/routes/scope_def/mod.rs -> src/routes/ch4_scope_def/mod.rs 	renamed:    src/routes/scope_def/reference.rs -> src/routes/ch4_scope_def/reference.rs 	renamed:    src/routes/scope_def/scope.rs -> src/routes/ch4_scope_def/scope.rs 	renamed:    src/routes/scope_def/slice.rs -> src/routes/ch4_scope_def/slice.rs 	renamed:    src/routes/struct_def/mod.rs -> src/routes/ch5_struct_def/mod.rs 	renamed:    src/routes/struct_def/struct_method.rs -> src/routes/ch5_struct_def/struct_method.rs 	renamed:    src/routes/enum_def/enum_ip.rs -> src/routes/ch6_enum_def/enum_ip.rs 	renamed:    src/routes/enum_def/mod.rs -> src/routes/ch6_enum_def/mod.rs 	modified:   src/routes/mod.rs

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,11 +1,11 @@
-mod guess;
-mod general_def;
-mod scope_def;
-mod struct_def;
-mod enum_def;
+mod ch2_guess;
+mod ch3_general_def;
+mod ch4_scope_def;
+mod ch5_struct_def;
+mod ch6_enum_def;
 
-pub use guess::*;
-pub use general_def::*;
-pub use scope_def::*;
-pub use struct_def::*;
-pub use enum_def::*;
+pub use ch2_guess::*;
+pub use ch3_general_def::*;
+pub use ch4_scope_def::*;
+pub use ch5_struct_def::*;
+pub use ch6_enum_def::*;
